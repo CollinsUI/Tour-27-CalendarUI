a810aa5acfd968edae1f78a232767222314b292e
Collins Swift <91741002+CollinsUI@users.noreply.github.com>
2022-11-22 11:52:49 +0300

    Initial Commit

---
+++ Tour 27 CalendarUI/Tour_27_CalendarUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tour_27_CalendarUIApp.swift
+//  Tour 27 CalendarUI
+//
+//  Created by Collins Muthomi on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Tour_27_CalendarUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
